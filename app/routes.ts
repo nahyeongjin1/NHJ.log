0ea209d081a74ec95d63ee2bc4e7d16ccf33eee2
나형진 <skgudwls@konkuk.ac.kr>
2025-12-03 20:33:07 +0900

    feat: /blog 라우트 등록

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
   index('routes/home.tsx'),
+  route('blog', 'routes/blog.tsx'),
   route('design-test', 'routes/design-test.tsx'),
   route('examples', 'routes/examples.tsx'),
   route('action/set-theme', 'routes/action/set-theme.ts'),
