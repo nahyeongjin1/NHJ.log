fffa7cb15e4c53840cf0f31329c0438676b10c88
나형진 <skgudwls@konkuk.ac.kr>
2025-12-27 20:00:15 +0900

    feat: 동적 라우팅 & static prerendering 설정

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,21 @@
 import type { Config } from '@react-router/dev/config';
+import { init } from 'react-router-mdx/server';
+
+const mdx = init({ path: 'content/posts', alias: 'posts' });
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   future: {
     v8_middleware: true,
+  },
+  async prerender() {
+    return [
+      '/',
+      '/posts',
+      '/projects',
+      '/bookmarks',
+      '/about',
+      ...(await mdx.paths()),
+    ];
   },
 } satisfies Config;
