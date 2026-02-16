0eee165c26828b74e4dcc6549065773717d69269
나형진 <skgudwls@konkuk.ac.kr>
2026-02-16 18:28:44 +0900

    refactor: 이미지 원본 비율 유지 & 가운데 정렬

--- app/components/mdx/Image.tsx
+++ app/components/mdx/Image.tsx
@@ -6,7 +6,12 @@ interface ImageProps {
 export function Image({ src, alt = '' }: ImageProps) {
   return (
     <figure className="my-6">
-      <img src={src} alt={alt} loading="lazy" className="w-full rounded-lg" />
+      <img
+        src={src}
+        alt={alt}
+        loading="lazy"
+        className="max-w-full w-auto mx-auto rounded-lg"
+      />
       {alt && (
         <figcaption className="mt-2 text-center text-sm text-secondary">
           {alt}
